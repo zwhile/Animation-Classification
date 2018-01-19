343c6cc5befeffd274e2e3b2cf29bb744cdbf037
zwhile <zackwhile@outlook.com>
2018-01-19 18:44:37 -0500

    Fixed syntax errors.

--- loading-data.py
+++ loading-data.py
@@ -90,31 +90,31 @@ def main(argv):
 #----------------------------------------------------------------------------------------       
     for epoch in range(2):  # loop over the dataset multiple times
 
-    running_loss = 0.0
-    for i, data in enumerate(dataset_loader, 0):
-        # get the inputs
-        inputs, labels = data
+        running_loss = 0.0
+        for i, data in enumerate(dataset_loader, 0):
+            # get the inputs
+            inputs, labels = data
+    
+            # wrap them in Variable
+            inputs, labels = Variable(inputs), Variable(labels)
+    
+            # zero the parameter gradients
+            optimizer.zero_grad()
+    
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+    
+            # print statistics
+            running_loss += loss.data[0]
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
 
-        # wrap them in Variable
-        inputs, labels = Variable(inputs), Variable(labels)
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.data[0]
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-
-print('Finished Training')
+    print('Finished Training')
 
 if __name__ == "__main__":
     main(sys.argv)
