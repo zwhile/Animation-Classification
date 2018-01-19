4580d565d1b33acfba0acd3f3f2ac184d89e3835
zwhile <zackwhile@outlook.com>
2018-01-19 17:53:11 -0500

    Further testing why imsave isn't working.

--- loading-data.py
+++ loading-data.py
@@ -14,30 +14,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data_transform = transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
+        #transforms.RandomResizedCrop(224),
+        #transforms.RandomHorizontalFlip(),
+        #transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
     ])
-cartoon_dataset = datasets.ImageFolder(root='data',
-                                           transform=data_transform)
+cartoon_dataset = datasets.ImageFolder(root='data')
+                                       #,transform=data_transform)
 dataset_loader = torch.utils.data.DataLoader(cartoon_dataset,
                                              batch_size=4, shuffle=True,
                                              num_workers=4)
 classes = ('BillyMandy', 'Chowder', 'EdEddEddy', 'Fosters', 'Lazlo')
 
 def imshow(img):
-    norm_img = img
-    img = img / 2 + 0.5     # unnormalize
+    #norm_img = img
+    #img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     #print(np.transpose(npimg, (1, 2, 0)))
     print("max unnorm: {}".format(np.amax(np.transpose(npimg, (1, 2, 0)))))
     print("min unnorm: {}".format(np.amin(np.transpose(npimg, (1, 2, 0)))))
     print()
-    npimg = norm_img.numpy()
-    print("max norm: {}".format(np.amax(np.transpose(npimg, (1, 2, 0)))))
-    print("min norm: {}".format(np.amin(np.transpose(npimg, (1, 2, 0)))))
+    #npimg = norm_img.numpy()
+    #print("max norm: {}".format(np.amax(np.transpose(npimg, (1, 2, 0)))))
+    #print("min norm: {}".format(np.amin(np.transpose(npimg, (1, 2, 0)))))
     #plt.imsave("foo.png", np.transpose(npimg, (1, 2, 0)))
 
 
