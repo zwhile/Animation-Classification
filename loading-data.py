34a6c67f964ac96f18be0f1db37b1c00fbb75ee6
zwhile <zackwhile@outlook.com>
2018-01-19 17:53:56 -0500

    Further testing why imsave isn't working.

--- loading-data.py
+++ loading-data.py
@@ -16,9 +16,9 @@ import numpy as np
 data_transform = transforms.Compose([
         #transforms.RandomResizedCrop(224),
         #transforms.RandomHorizontalFlip(),
-        #transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
+        transforms.ToTensor(),
+        #transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             #std=[0.229, 0.224, 0.225])
     ])
 cartoon_dataset = datasets.ImageFolder(root='data')
                                        #,transform=data_transform)
